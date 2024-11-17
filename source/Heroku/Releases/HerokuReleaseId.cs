d67b1badc21f01b32af26a8f2dc19df9aab6ebef
Patchouli <self@patchouli.sh>
2024-11-17 13:47:10 -0500

    Fix .gitignore and missing source code

---
+++ source/Heroku/Releases/HerokuReleaseId.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+namespace Heroku.Releases
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[StructLayout(
+		LayoutKind.Sequential)]
+	[JsonConverter(
+		typeof(HerokuReleaseIdJsonConverter))]
+	public readonly struct HerokuReleaseId :
+		IEquatable<HerokuReleaseId>
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="valueA"></param>
+		/// <param name="valueB"></param>
+		/// <returns></returns>
+		public static bool Equals(
+			HerokuReleaseId valueA,
+			HerokuReleaseId valueB)
+		{
+			return
+				(valueA.value == valueB.value);
+		}
+
+		private readonly Guid value;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public Guid Value =>
+			this.value;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="value"></param>
+		public HerokuReleaseId(
+			Guid value)
+		{
+			this.value = value;
+		}
+
+		/// <inheritdoc/>
+		public bool Equals(
+			HerokuReleaseId other) =>
+				Equals(
+					this,
+					other);
+
+		/// <inheritdoc/>
+		public override bool Equals(
+			object obj)
+		{
+			if (obj is HerokuReleaseId other)
+			{
+				return Equals(
+					this,
+					other);
+			}
+
+			return false;
+		}
+
+		/// <inheritdoc/>
+		public override int GetHashCode() =>
+			this.value.GetHashCode();
+
+		/// <inheritdoc/>
+		public override string ToString() =>
+			this.value.ToString();
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="value"></param>
+		[MethodImpl(
+			MethodImplOptions.AggressiveInlining)]
+		public static implicit operator Guid(
+			HerokuReleaseId value) =>
+				value.Value;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="valueLeft"></param>
+		/// <param name="valueRight"></param>
+		/// <returns></returns>
+		[MethodImpl(
+			MethodImplOptions.AggressiveInlining)]
+		public static bool operator ==(
+			HerokuReleaseId valueLeft,
+			HerokuReleaseId valueRight) =>
+				Equals(
+					valueLeft,
+					valueRight);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="valueLeft"></param>
+		/// <param name="valueRight"></param>
+		/// <returns></returns>
+		[MethodImpl(
+			MethodImplOptions.AggressiveInlining)]
+		public static bool operator !=(
+			HerokuReleaseId valueLeft,
+			HerokuReleaseId valueRight) =>
+				!Equals(
+					valueLeft,
+					valueRight);
+	}
+}
---
+++ source/Heroku/Releases/HerokuReleaseIdJsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Heroku.Releases
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	internal sealed class HerokuReleaseIdJsonConverter
+		: JsonConverter<HerokuReleaseId>
+	{
+		/// <inheritdoc/>
+		public override HerokuReleaseId Read(
+			ref Utf8JsonReader reader,
+			Type typeToConvert,
+			JsonSerializerOptions options) =>
+				new HerokuReleaseId(
+					reader.GetGuid());
+
+		/// <inheritdoc/>
+		public override void Write(
+			Utf8JsonWriter writer,
+			HerokuReleaseId value,
+			JsonSerializerOptions options) =>
+				writer.WriteStringValue(
+					value.Value);
+	}
+}
