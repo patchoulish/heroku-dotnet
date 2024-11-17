d095dfcbf2c8cad7bbcd7e99064b775622c778ba
Patchouli <self@patchouli.sh>
2024-11-17 13:43:28 -0500

    Add unit test initialization

---
+++ source/Heroku.Tests/HerokuServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Microsoft;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.TestTools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Heroku
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[TestClass]
+	public sealed partial class HerokuServiceTests
+	{
+		private IHerokuService heroku;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		[TestInitialize]
+		public void Initialize()
+		{
+			this.heroku =
+				new HerokuService(
+					Environment.GetEnvironmentVariable(
+						"HEROKU_API_KEY"));
+		}
+	}
+}
